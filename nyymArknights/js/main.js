381706677152f1f1cc2113d6d8fbd7f37ea784c5
轩川浮华 <1246305034@qq.com>
2022-04-09 23:57:40 +0800

    JQ增加easing拓展

--- nyymArknights/js/main.js
+++ nyymArknights/js/main.js
@@ -9,16 +9,16 @@ $(function (){
 	setInterval(function (){
 		num++;
 		let rn = num % 3;// 求余
-		$('#fullSlide').animate({scrollTop: option[rn]},"easy","swing");
+		$('#fullSlide').animate({scrollTop: option[rn]},{easing: 'easeInOutQuad',duration: 750});
 	},5000)// 5秒切换一次
 })
 // 大图自动滚动 END
 // 推荐作品页面切换按钮监听 START
 $("input:radio[name='pages']").change(function(){
 	if($(this).val() == 1){
-		$('#recommend-overflowHiddin').animate({scrollLeft: 0},"easy","swing")
+		$('#recommend-overflowHiddin').animate({scrollLeft: 0},{easing: 'easeInOutQuad',duration: 500})
 	}else {
-		$('#recommend-overflowHiddin').animate({scrollLeft: 1200},"easy","swing")
+		$('#recommend-overflowHiddin').animate({scrollLeft: 1200},{easing: 'easeInOutQuad',duration: 500})
 	}
 })
 // 推荐作品页面切换按钮监听 END

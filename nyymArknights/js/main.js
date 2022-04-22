738cb2f88fcedb8244c9ac0a3f172fd1b1d37cd1
轩川浮华 <1246305034@qq.com>
2022-04-23 01:50:55 +0800

    评论区完善

--- nyymArknights/js/main.js
+++ nyymArknights/js/main.js
@@ -1,5 +1,10 @@
 
 $(function (){
+	// 设置动画默认为开启
+	if (!localStorage["animate"]){
+		// 如果不存在animate参数则设置为true
+		localStorage["animate"] = "true";
+	}
 	// 大图自动滚动 START
 	let auto_scroll_option = {
 		"0": 0,// 第一张图的位置
@@ -50,14 +55,14 @@ $('input:radio[name="comment-type"]').change(function () {
 	// 根据ID来写入ID2
 	if(id === "long-comment") {
 		id2 = "short-comment";
-		$("#comment-box .comment-input").fadeOut("slow");
+		$("#comment-box .comment-input").css("display","none");
 	}else if (id === "short-comment"){
 		id2 = "long-comment";
-		$("#comment-box .comment-input").fadeIn("slow");
+		$("#comment-box .comment-input").css("display","block");
 	}
 	// 执行短评和长评相关元素的隐藏与显示
-	$("#comment-box .left>." + id2).fadeOut("slow");
-	$("#comment-box .left>." + id).fadeIn("slow");
+	$("#comment-box .left>." + id2).css("display","none");
+	$("#comment-box .left>." + id).css("display","block");
 })
 // 短评和长评切换监听 E N D
 
@@ -71,7 +76,7 @@ $('#comment-box .comment-input textarea').on("keyup",function () {
 // 评论框输入长度显示 E N D
 
 // 评论选项按钮监听 START
-$("#comment-box .user-info-box i").on("click",function () {
+$("#comment-box").on("click",".user-info-box i",function () {
 	// 选中所有show=on的元素，淡出隐藏
 	$("#comment-box div[show='on']").fadeOut("fast",function () {
 		// 动画完成后删除show参数
@@ -103,4 +108,118 @@ $("#comment-box .user-info-box i").on("click",function () {
 		});
 	}
 })
+// 评论选项点击后监听执行
+$("#comment-box .option-list span").on("click", function () {
+	// 获取被点击的span是什么
+	let this_class = $(this).attr("class");
+	//  功能分别判断
+	if (this_class === "report") {
+		// 举报
+		alert("已举办（功能尚未实现）");
+	}else if (this_class === "black-list") {
+		// 黑名单
+		alert("已加入黑名单（功能尚未实现）");
+	}
+	// 点击后将父元素隐藏
+	$(this).parent().fadeOut("fast");
+})
 // 评论选项按钮监听 E N D
+// 显示更多评论监听 START
+$("#comment-box .cmt-more a").on("click",function () {
+	// 将按钮改为灰色
+	$(this).css("background-color", "#898989");
+	// 找到最后一个评论元素
+	let cmt_content = $(this).parent().prev();
+	let cmt_json = {
+		"user-img": "https://rss.sfacg.com/web/account/images/avatars/upload/APP/1605/1463479732757.jpg",
+		"user-name": "不是轻小说",
+		"user-uid": "1",
+		"user-tag": [
+			{
+				"class": "user-tag-admin",
+				"name": "管理员"
+			},
+			{
+				"class": "user-tag-notice",
+				"name": "公告"
+			}
+		],
+		"cmt-time": "1145-01-04 19:19:10",
+		"cmt-content": "评论系统...力竭了",
+		"cmt-like": "0001",
+		"cmt-comment": {
+			"count": "0002",
+			"comment": [
+				{
+					"user-img": "https://rss.sfacg.com/web/account/images/avatars/upload/APP/1605/1463479732757.jpg",
+					"user-name": "SF轻小说1",
+					"user-uid": "2",
+					"user-tag": [
+						{
+							"class": "user-tag-admin",
+							"name": "管理员"
+
+						}
+					],
+					"cmt-time": "1145-01-04 19:19:11",
+					"cmt-content": "永远维护中。。。",
+				},
+				{
+					"user-img": "https://rss.sfacg.com/web/account/images/avatars/upload/APP/1605/1463479732757.jpg",
+					"user-name": "SF轻小说2",
+					"user-uid": "3",
+					"user-tag": [
+						{
+							"class": "user-tag-admin",
+							"name": "管理员"
+
+						}
+					],
+					"cmt-time": "1145-01-04 19:19:50",
+					"cmt-content": "哼嗯嗯啊啊啊",
+				}
+			]
+		}
+	}
+	setTimeout(function () {
+		// 创建user_tag的html字符串
+		let user_tags = "";
+		// 遍历json中的tag
+		for (let i = 0; i < cmt_json["user-tag"].length; i++) {
+			user_tags += '<span class="user-tag '+ cmt_json["user-tag"][i]["class"] +'">'+ cmt_json["user-tag"][i]["name"] +'</span>'
+		}
+		// 要插入的html
+		let html = '<div class="cmt-box" user-uid="'+ cmt_json["user-uid"] + '">' +
+				'<div class="user-info-box">' +
+					'<img class="user-img" src="'+ cmt_json["user-img"] +'" alt="'+ cmt_json["user-name"] + "头像" +'">' +
+					'<div class="user-info">' +
+						'<p>' +
+							'<span class="user-name">'+ cmt_json["user-name"] +'</span>' +
+							user_tags +
+						'</p>' +
+						'<span class="cmt-time">'+ cmt_json["cmt-time"] +'</span>' +
+					'</div>' +
+					'<i class="fa fa-ellipsis-v"></i>' +
+					'<div class="option-list">' +
+						'<span class="black-list">加入黑名单</span>' +
+						'<span class="report">举报</span>' +
+					'</div>' +
+				'</div>' +
+				'<div class="cmt-content">' +
+					cmt_json["cmt-content"] +
+				'</div>' +
+				'<div class="cmt-item">' +
+					'<i class="fa fa-1p25x fa-thumbs-up" title="点赞">('+ cmt_json["cmt-like"] +')</i>' +
+					'<i class="fa fa-1p25x fa-comment" title="回复">('+ cmt_json["cmt-comment"]["count"] +')</i>' +
+				'</div>' +
+			'</div>'
+		// 执行显示更多评论
+		cmt_content.after(
+			// 要显示的评论
+			html
+		);
+		// 成功显示后将按钮颜色改回
+		$("#comment-box .cmt-more a").css("background-color","");
+	},1000)
+})
+// 显示更多评论监听 E N D
